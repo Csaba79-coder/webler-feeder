1a52e31fdfce63ed4be5e8c00f348c2f575910ca
webler-biology-physiology <csabavadasz79@gmail.com>
2024-02-03 12:38:07 +0100

    Initial commit

---
+++ src/main/java/hu/webler/weblerfeeder/WeblerFeederApplication.java
@@ -0,0 +1,13 @@
+package hu.webler.weblerfeeder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblerFeederApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeblerFeederApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/webler/weblerfeeder/WeblerFeederApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.webler.weblerfeeder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblerFeederApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
