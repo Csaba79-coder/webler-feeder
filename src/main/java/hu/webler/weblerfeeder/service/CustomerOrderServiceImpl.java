c033cef264e33768b1a6cae3332556d407673211
gyepike <antipet@gmail.com>
2024-03-09 12:08:41 +0100

    Created Service + Service Impl

---
+++ src/main/java/hu/webler/weblerfeeder/service/CustomerOrderServiceImpl.java
@@ -0,0 +1,34 @@
+package hu.webler.weblerfeeder.service;
+
+import hu.webler.weblerfeeder.entity.CustomerOrder;
+
+import java.util.List;
+
+public class CustomerOrderServiceImpl implements CustomerOrderService{
+
+
+    @Override
+    public List<CustomerOrder> getAllCustomerOrder() {
+        return null;
+    }
+
+    @Override
+    public CustomerOrder saveCustomerOrder(CustomerOrder order) {
+        return null;
+    }
+
+    @Override
+    public CustomerOrder deleteCustomerOrder(Long id) {
+        return null;
+    }
+
+    @Override
+    public CustomerOrder findCustomerOrderById(Long id) {
+        return null;
+    }
+
+    @Override
+    public CustomerOrder findCustomerOrderByAddress(String address) {
+        return null;
+    }
+}
