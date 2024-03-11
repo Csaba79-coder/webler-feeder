c487fa0f211b6f215236a5222df49b917e9e763d
Ábrahám Miklós <mikcsek2@gmail.com>
2024-03-11 16:47:11 +0100

    Fill the Mapper with data about Costumer

--- src/main/java/hu/webler/weblerfeeder/util/Mapper.java
+++ src/main/java/hu/webler/weblerfeeder/util/Mapper.java
@@ -1,4 +1,33 @@
 package hu.webler.weblerfeeder.util;
 
+import hu.webler.weblerfeeder.entity.Customer;
+import hu.webler.weblerfeeder.model.CustomerCreateModel;
+import hu.webler.weblerfeeder.model.CustomerModel;
+
 public class Mapper {
+
+    public static CustomerModel mapCustomerEntityToCustomerModel(Customer customer) {
+        CustomerModel customerModel = new CustomerModel();
+        customerModel.setId(customer.getId());
+        customerModel.setFirstName(customer.getFirstName());
+        customerModel.setMidName(customer.getMidName());
+        customerModel.setLastName(customer.getLastName());
+        customerModel.setCell(customer.getCell());
+        customerModel.setEmail(customer.getEmail());
+        customerModel.setRegistrationDate(customer.getRegistrationDate());
+        customerModel.setStatus(customer.getStatus());
+        return customerModel;
+    }
+
+    public static Customer mapCustomerCreateModelToCustomerEntity(CustomerCreateModel customerCreateModel) {
+        Customer customer = new Customer();
+        customer.setFirstName(customerCreateModel.getFirstName());
+        customer.setMidName(customerCreateModel.getMidName());
+        customer.setLastName(customerCreateModel.getLastName());
+        customer.setCell(customerCreateModel.getCell());
+        customer.setEmail(customerCreateModel.getEmail());
+        customer.setRegistrationDate(customerCreateModel.getRegistrationDate());
+        customer.setStatus(customerCreateModel.getStatus());
+        return customer;
+    }
 }
