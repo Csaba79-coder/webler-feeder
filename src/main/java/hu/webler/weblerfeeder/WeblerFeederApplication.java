586d700a1c7b9f9d9b78b8c62b1eae4c6b790932
gyepike <antipet@gmail.com>
2024-03-22 16:04:47 +0100

    Create Initial

--- src/main/java/hu/webler/weblerfeeder/WeblerFeederApplication.java
+++ src/main/java/hu/webler/weblerfeeder/WeblerFeederApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WeblerFeederApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(WeblerFeederApplication.class, args);
 	}
