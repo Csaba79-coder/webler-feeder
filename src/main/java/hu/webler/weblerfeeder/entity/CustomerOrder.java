a1434675bd07e2d0a9a1af095e19ab69b8b30d4e
gyepike <antipet@gmail.com>
2024-03-22 16:16:25 +0100

    Create Initial

---
+++ src/main/java/hu/webler/weblerfeeder/entity/CustomerOrder.java
@@ -0,0 +1,34 @@
+package hu.webler.weblerfeeder.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerOrder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String address;
+
+    @CreationTimestamp
+    private LocalDateTime orderDate = LocalDateTime.now();
+
+    private String description;   // kapucsengő emelett stb.
+
+    // customer + food customerOrder one-one, food customerorder one-to-many
+}
