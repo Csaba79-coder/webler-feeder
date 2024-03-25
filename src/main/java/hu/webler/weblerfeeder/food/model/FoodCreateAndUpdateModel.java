b588bff7e9c19a0e9579d1e0f29bce3929fad076
gyepike <antipet@gmail.com>
2024-03-25 15:40:37 +0100

    add id and creation time for Food & Order

--- src/main/java/hu/webler/weblerfeeder/food/model/FoodCreateAndUpdateModel.java
+++ src/main/java/hu/webler/weblerfeeder/food/model/FoodCreateAndUpdateModel.java
@@ -5,12 +5,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class FoodCreateAndUpdateModel {
 
+    private Long id;
+    private LocalDateTime createdAt;
     private String name;
     private String description;
     private Double price;
