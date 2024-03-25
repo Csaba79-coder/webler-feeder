9772a90c63d2f8b8277135fcd710a6346ea2c925
gyepike <antipet@gmail.com>
2024-03-25 14:14:48 +0100

    Added Lombok Annotation

--- src/main/java/hu/webler/weblerfeeder/order/model/OrderCreateModel.java
+++ src/main/java/hu/webler/weblerfeeder/order/model/OrderCreateModel.java
@@ -1,5 +1,14 @@
 package hu.webler.weblerfeeder.order.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderCreateModel {
 
     private String address;
--- src/main/java/hu/webler/weblerfeeder/order/model/OrderUpdateModel.java
+++ src/main/java/hu/webler/weblerfeeder/order/model/OrderUpdateModel.java
@@ -2,6 +2,15 @@ package hu.webler.weblerfeeder.order.model;
 
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderUpdateModel {
 
     private LocalDateTime createdAt;
